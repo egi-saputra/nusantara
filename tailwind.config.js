b0e6756290d71de3fc470c192ed7bdac9a23b4f8
egi-saputra <hanalikaproject@gmail.com>
2026-01-24 20:01:12 +0700

    P

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
     darkMode: "class",
     content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                carousel: "carousel 12s linear infinite",
+            },
+            keyframes: {
+                carousel: {
+                    "0%, 25%": { transform: "translateX(0%)" },
+                    "30%, 50%": { transform: "translateX(-100%)" },
+                    "55%, 75%": { transform: "translateX(-200%)" },
+                    "80%, 100%": { transform: "translateX(-300%)" },
+                },
+            },
+        },
     },
     plugins: [],
 };
